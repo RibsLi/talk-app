c0bb39c16210233d6abfae259f24292ff6f968d1
RibsLi <2234692411@qq.com>
2023-05-06 16:05:00 +0800

    测试 WebSocket

--- src/pages/index/home.vue
+++ src/pages/index/home.vue
@@ -4,7 +4,7 @@
     <scroll-view scroll-y class="h-full">
       <uni-swipe-action>
         <uni-swipe-action-item v-for="item in list" :key="item" :threshold="0" :right-options="options" @click="deleteEv">
-          <view class="p-20">
+          <view class="px-20 pt-20">
             <DialogueCard :content="item" />
           </view>
         </uni-swipe-action-item>
@@ -30,7 +30,26 @@ const list = ref([
   { name: '德玛西亚', type: 'friend', avatar: 'https://tse1-mm.cn.bing.net/th/id/OET.69e3368a10d64e6c9d422a8cad70723b?w=272&h=272&c=7&rs=1&o=5&pid=1.9', message: '德玛西亚万岁，人在塔在。', count: 100 }
 ])
 
+const socket = new WebSocket('ws://127.0.0.1:3100')
+
 function deleteEv() {
   console.log('删除事件')
+  const obj = {
+    name: 'zs',
+    date: new Date().getDate()
+  }
+  socket.send(JSON.stringify(obj))
+}
+
+socket.onclose = e => {}
+
+socket.onerror = e => {}
+
+socket.onmessage = e => {
+  console.log('onmessage', JSON.parse(e.data))
+}
+
+socket.onopen = e => {
+  console.log('onopen', e)
 }
 </script>
